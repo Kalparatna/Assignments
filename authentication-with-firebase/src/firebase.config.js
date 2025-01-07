8376c10c14250b7be6589e39078fb4805099dda0
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-01-07 18:42:27 +0530

    Update firebase.config.js

--- authentication-with-firebase/src/firebase.config.js
+++ authentication-with-firebase/src/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSSTnkVrJwxorU4hCvNl7joFQNz34Vwuc",
-  authDomain: "authenticationapp-33783.firebaseapp.com",
-  projectId: "authenticationapp-33783",
-  storageBucket: "authenticationapp-33783.firebasestorage.app",
-  messagingSenderId: "1059998159314",
-  appId: "1:1059998159314:web:ea85ec601b2d36bcf3f80b",
-  measurementId: "G-G06NW0DFPX"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
