f51ed84b2f65f51db2c483f9c6b90ae2c2ea4340
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-01-09 11:11:31 +0530

    Add files via upload

---
+++ Nodemailer/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config(); 
+const nodemailer = require('nodemailer'); 
+
+const mailOptions = {
+    from: process.env.EMAIL_USER, 
+    to: 'indianteam727@gmail.com', 
+    subject: "Sending Email to Rajat", 
+    text: "Welcome to NodeMailer, It's Working", 
+    html: '<h1>Welcome</h1><p>That was easy!</p>' 
+};
+
+// Transporter setup using Gmail service
+const transporter = nodemailer.createTransport({
+    service: 'gmail', 
+    auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS  
+    }
+});
+
+// Send the email
+transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+        console.error("Error while sending email:", error);
+    } else {
+        console.log('Email sent successfully:', info.response); 
+    }
+});
